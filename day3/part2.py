0149e1d245ad0387856cef8b9cfa0f61a4fd4dfa
poveilleux <poveilleux@users.noreply.github.com>
2022-12-09 11:58:30 -0500

    Create part2.py

---
+++ day3/part2.py
@@ -0,0 +1,16 @@
+file = open("puzzle.txt", "r")
+lines = file.readlines()
+
+total = 0
+for i in range(0, len(lines), 3):
+    elf1, elf2, elf3 = lines[i], lines[i + 1], lines[i + 2]
+
+    for c in elf1:
+        if elf2.find(c) > -1 and elf3.find(c) > -1:
+            if c >= 'a' and c <= 'z':
+                total += ord(c) - 96
+            if c >= 'A' and c <= 'Z':
+                total += ord(c) - 38
+            break;
+
+print("Total:", total)
