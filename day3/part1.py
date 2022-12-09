8923822c82f1d57b3363147802a034a8cacc99b8
poveilleux <poveilleux@users.noreply.github.com>
2022-12-09 11:46:11 -0500

    Create part1.py

---
+++ day3/part1.py
@@ -0,0 +1,16 @@
+file = open("puzzle.txt", "r")
+lines = file.readlines()
+
+total = 0
+for line in lines:
+    part1, part2 = line[:len(line)//2], line[len(line)//2:]
+
+    for c in part1:
+        if part2.find(c) > -1:
+            if c >= 'a' and c <= 'z':
+                total += ord(c) - 96
+            if c >= 'A' and c <= 'Z':
+                total += ord(c) - 38
+            break;
+
+print("Total:", total)
