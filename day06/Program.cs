e0e259274c7a0efb9a5e9c984f320c704d1f51c9
🦔 Pierre-Olivier Veilleux <poveilleux@GENETEC.COM>
2022-12-29 17:48:43 -0500

    part 2

---
+++ day06/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+string input = File.ReadAllText("input.txt");
+
+Part1(input);
+Part2(input);
+
+void Part1(string input)
+{
+    int skip = 0;
+    while (true)
+    {
+        IEnumerable<char> part = input.Skip(skip).Take(4);
+        if (part.Count() == part.Distinct().Count())
+        {
+            Console.WriteLine("Result for part 1 is: {0}", skip + 4);
+            return;
+        }
+
+        skip++;
+    }
+}
+
+void Part2(string input)
+{
+    int skip = 0;
+    while (true)
+    {
+        IEnumerable<char> part = input.Skip(skip).Take(14);
+        if (part.Count() == part.Distinct().Count())
+        {
+            Console.WriteLine("Result for part 2 is: {0}", skip + 14);
+            return;
+        }
+
+        skip++;
+    }
+}
--- day06/README.md
+++ day06/README.md
@@ -1,6 +1,6 @@
 # Day 6 - C#
 Challenge: https://adventofcode.com/2022/day/6
 
-Part 1: 
+Part 1: 1300
 
 Part 2: 
