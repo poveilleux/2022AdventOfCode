bd0d109f8333725aef324037c46d7be0a22c96e4
🦔 Pierre-Olivier Veilleux <poveilleux@GENETEC.COM>
2023-01-01 17:57:44 -0500

    parsing addx works

--- day10/part1.hs
+++ day10/part1.hs
@@ -1,13 +1,27 @@
 import System.IO (isEOF)
-import Data.Int
 
-myFunc acc = do
+-- Source: https://stackoverflow.com/a/10967044
+split :: Eq a => a -> [a] -> [[a]]
+split x y = func x y [[]]
+    where
+        func x [] z = reverse $ map (reverse) z
+        func x (y:ys) (z:zs) = if y==x then
+            func x ys ([]:(z:zs))
+        else
+            func x ys ((y:z):zs)
+
+myFunc cycle = do
     done <- isEOF
     if done then
-        putStrLn ("The result for part 1 is " ++ (show acc))
+        putStrLn ("The result for part 1 is " ++ (show cycle))
     else do
         line <- getLine
-        print line
-        myFunc (acc + 1)
+        if line == "noop" then
+            putStrLn "FOO"
+        else do
+            let numAsStr = (split ' ' line)!!1
+            let add = read numAsStr :: Integer
+            print add
+        myFunc (cycle + 1)
 
 main = myFunc 1
